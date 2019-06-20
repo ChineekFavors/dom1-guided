7ac6de84171f1dfc608e9285871c28bd71471b74
Chineek Favors <chineekfavors01@gmail.com>
2019-06-19 21:57:52 -0700

    added commits to my code

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Connected to the console!');
+//console.log('Connected to the console!');
+//query select nav lins and change text color to brown
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(links => links.style.color = 'brown');
+
+//eventListener mouseover logo image disappears
+const logoImage = document.querySelector('div.logo-container img');
+logoImage.addEventListener('mouseover', () =>{
+	logoImage.src = '';
+	
+});
+
+const btn = document.querySelector('#custom-btn');
+btn.addEventListener('click', () =>{
+	btn.textContent = 'and We will work with you!';
+})
